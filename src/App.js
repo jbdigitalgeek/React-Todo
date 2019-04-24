9fa12eda70f18fa1aac8030db1d4f9e9d8d9a600
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-04-24 14:11:43 -0700

    input starter data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
 
+
 class App extends React.Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Clean Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Cancel Gym Membership',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  addTodo = props => {
+    props.preventDefault();
+    const newTodo = { task: this.state.todo, completed: false, id: Date.now() };
+    this.setState({
+      todos: [...this.state.todos, newTodo], todo: ''
+    });
+    
+  };
+  
+  
+  
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+      <div></div>
     );
   }
 }
